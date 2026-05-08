9c6c0c05675577b4a8f93ec28454f2af88d9fc9c
Matt Clarke <matto.kuraku@gmail.com>
2026-05-08 07:23:51 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,22 @@ use crate::gilded_rose::GildedRose;
 
 fn main() {
     let items = vec![
-        Item::new("+5 Dexterity Vest", 10, 20),
-        Item::new("Aged Brie", 2, 0),
+        Item::new("+5 Dexterity Vest", 5, 15),
         Item::new("Elixir of the Mongoose", 5, 7),
+        Item::new("Aged Brie", 3, 0),
+        Item::new("Aged Brie", 2, 45),
         Item::new("Sulfuras, Hand of Ragnaros", 0, 80),
         Item::new("Sulfuras, Hand of Ragnaros", -1, 80),
         Item::new("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-        Item::new("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-        Item::new("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+        Item::new("Backstage passes to a TAFKAL80ETC concert", 10, 30),
+        Item::new("Backstage passes to a TAFKAL80ETC concert", 5, 41),
         // this conjured item does not work properly yet
         Item::new("Conjured Mana Cake", 3, 6),
     ];
     let mut rose = GildedRose::new(items);
 
     println!("OMGHAI!");
-    for i in 0..=30 {
+    for i in 0..=10 {
         println!("-------- day {} --------", i);
         println!("name, sellIn, quality");
         for item in &rose.items {
